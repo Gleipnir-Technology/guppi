54fc0673ceb6cbe27be6b01d2aacbe866d36b4d0
Eli Ribble <eli@theribbles.org>
2025-06-06 05:00:57 -0700

    Use working example from genai repo
    And I do indeed get back an answer.

--- go.mod
+++ go.mod
@@ -5,12 +5,18 @@ go 1.24.1
 toolchain go1.24.4
 
 require (
+	github.com/andybalholm/brotli v1.1.1 // indirect
 	github.com/bahlo/generic-list-go v0.2.0 // indirect
 	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/invopop/jsonschema v0.13.0 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/mailru/easyjson v0.9.0 // indirect
 	github.com/maruel/genai v0.0.0-20250606014031-67fb370d6967 // indirect
+	github.com/maruel/httpjson v0.4.2 // indirect
+	github.com/maruel/roundtrippers v0.3.2 // indirect
 	github.com/wk8/go-ordered-map/v2 v2.1.8 // indirect
 	golang.org/x/sync v0.12.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
+
+replace github.com/maruel/genai => ../genai
--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+
+	"github.com/maruel/genai"
+	"github.com/maruel/genai/gemini"
+	"github.com/maruel/genai/genaitools"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	// Supported by Anthropic, Cerebras, Cloudflare, Cohere, DeepSeek, Gemini, Groq, HuggingFace, Mistral,
+	// Ollama, OpenAI, TogetherAI.
+
+	// Using a free small model for testing.
+	// See https://ai.google.dev/gemini-api/docs/models/gemini?hl=en
+	c, err := gemini.New("", "gemini-2.0-flash", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	msgs := genai.Messages{
+		genai.NewTextMessage(genai.User, "What is 3214 + 5632? Leverage the tool available to you to tell me the answer. Do not explain. Be terse. Include only the answer."),
+	}
+	opts := genai.OptionsText{
+		Tools: []genai.ToolDef{genaitools.Arithmetic},
+		// Force the LLM to do a tool call first.
+		ToolCallRequest: genai.ToolCallRequired,
+	}
+	newMsgs, _, err := genai.GenSyncWithToolCallLoop(context.Background(), c, msgs, &opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", newMsgs[len(newMsgs)-1].AsText())
 }
